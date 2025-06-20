f2c4100bbc15f00b092aa71bddb18b5ef7efa018
Ezéchiel Amen AGBLA <ezchilamen@gmail.com>
2025-06-20 10:34:16 +0200

    inital commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,24 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'register',
+    loadComponent: () => import('./pages/register/register.page').then( m => m.RegisterPage)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'todos',
+    loadComponent: () => import('./pages/todos/todos.page').then( m => m.TodosPage)
+  },
+  {
+    path: 'create-todo',
+    loadComponent: () => import('./pages/todos/create-todo/create-todo.page').then( m => m.CreateTodoPage)
+  },
+  {
+    path: 'edit-todo',
+    loadComponent: () => import('./pages/todos/edit-todo/edit-todo.page').then( m => m.EditTodoPage)
+  },
 ];
